af4dcfd9386d63d40e5c4a818f320feae4d4ae55
maxgjt <maxgong@yeah.net>
2019-05-07 18:47:07 +0800

    no message

--- Java2000_3/src/com/java2000/project/service/impl/OrderServiceImpl.java
+++ Java2000_3/src/com/java2000/project/service/impl/OrderServiceImpl.java
@@ -3,23 +3,25 @@ package com.java2000.project.service.impl;
 import com.java2000.project.bean.Order;
 import com.java2000.project.service.OrderService;
 import com.java2000.project.ui.BusinessException;
+import com.java2000.project.utils.OrderIO;
 
 import java.util.List;
 
 public class OrderServiceImpl implements OrderService {
+    private OrderIO orderIO = new OrderIO();
 
     @Override
     public void buyProduct(Order o) throws BusinessException {
-
+        orderIO.add(o);
     }
 
     @Override
     public List<Order> list() throws BusinessException {
-        return null;
+        return orderIO.list();
     }
 
     @Override
     public Order findById(int orderId) throws BusinessException {
-        return null;
+        return orderIO.findByorderId(orderId);
     }
 }
