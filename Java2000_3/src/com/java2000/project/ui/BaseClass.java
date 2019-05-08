d3adcd02eac6a212036b4cda7cd01eb1a8a1cc9c
maxgjt <maxgong@yeah.net>
2019-05-08 10:23:32 +0800

    no message

--- Java2000_3/src/com/java2000/project/ui/BaseClass.java
+++ Java2000_3/src/com/java2000/project/ui/BaseClass.java
@@ -1,6 +1,7 @@
 package com.java2000.project.ui;
 
 import com.java2000.project.bean.User;
+import com.java2000.project.framework.BeanFactory;
 
 import java.util.ResourceBundle;
 import java.util.Scanner;
@@ -8,6 +9,7 @@ import java.util.Scanner;
 public abstract class BaseClass {
     protected static Scanner input = new Scanner(System.in);
     protected static User currUser; //当前用户对象
+    protected BeanFactory beanFactory = null;
     private static ResourceBundle r = ResourceBundle.getBundle("com.java2000.project.res.r_temp");
 
     protected static String getString(String key){
@@ -16,5 +18,9 @@ public abstract class BaseClass {
 
     public static void print(String s){
         System.out.println(s);
+    }
+
+    public BaseClass(){
+        beanFactory = BeanFactory.init();
     }
 }
--- Java2000_3/src/com/java2000/project/ui/LoginClass.java
+++ Java2000_3/src/com/java2000/project/ui/LoginClass.java
@@ -5,12 +5,17 @@ import com.java2000.project.service.UserService;
 import com.java2000.project.service.impl.UserServiceImpl;
 
 public class LoginClass extends BaseClass {
+    private UserService userService;
+    public LoginClass(){
+        userService = (UserService) beanFactory.getBean("userService");
+    }
+
     public void login() throws BusinessException {
         print(getString("input.username"));
         String username = input.nextLine();
         print(getString("input.password"));
         String password = input.nextLine();
-        UserService userService = new UserServiceImpl();
+//        UserService userService = new UserServiceImpl();
         User user = userService.login(username, password);
         System.out.println(user);
         if (user!=null){
